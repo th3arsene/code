e19adc557dc853d46ab9a011ac5dbd384e76d482
ethan <ethan@ethanyu.com>
2018-11-29 09:33:48 -0500

    arduino paste

--- arduino uwu/hello_world/hello_world.ino
+++ arduino uwu/hello_world/hello_world.ino
@@ -1,9 +1,9 @@
-void setup() {
-  // put your setup code here, to run once:
-
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Hello World");
 }
-
-void loop() {
+  void loop() {
+   
   // put your main code here, to run repeatedly:
 
 }
